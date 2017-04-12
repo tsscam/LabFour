27a76abcedde864648ab70a130f2340265a83be5
Theresa Skonieczny <tsscamaro@gmail.com>
2017-04-12 19:42:30 -0400

    first commit lab 4

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab4b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Please enter any number between 1 and 10: ");
+            Convert.ToInt32(Console.ReadLine());
+           
+            {
+
+            }
+            Console.WriteLine("The factorial is '{0}' is '{1}'");
+            for (int i = 1; i < 10; i++)
+                Console.WriteLine (i);
+        }
+    }
+}
